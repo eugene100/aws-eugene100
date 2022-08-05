e881e92d0438c54ce316908ee048dd0907d2a99a
Yevhen Stolietnii <eugene100@users.noreply.github.com>
2022-08-05 17:53:13 +0300

    Upgrade to v1.2.6 (#2)

--- terraform-management/s3.tf
+++ terraform-management/s3.tf
@@ -2,7 +2,14 @@ data "aws_s3_bucket" "eugene100-terraform-tfstate" {
   bucket = "eugene100-terraform-tfstate"
 }
 
-resource "aws_s3_bucket" "k8s-state-store" {
-    bucket = "eugene100-k8s-state-store"
-    acl    = "private"
-}
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
+
+#   bucket = "eugene100-k8s-state-store"
+#   acl    = "private"
+
+#   versioning = {
+#     enabled = true
+#   }
+
+# }
--- terraform-management/vpc.tf
+++ terraform-management/vpc.tf
@@ -1,80 +1,18 @@
-resource "aws_vpc" "vpc-default" {
-    cidr_block           = "172.31.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    instance_tenancy     = "default"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-    tags {
-      Name = "default"
-      managed = "terraform"
-      environment = "management"
-    }
-}
+  name = "management-vpc"
+  cidr = "10.0.0.0/16"
 
-# Subnets
-resource "aws_subnet" "subnet-default-1a" {
-    vpc_id                  = "${aws_vpc.vpc-default.id}"
-    cidr_block              = "172.31.80.0/20"
-    availability_zone       = "${var.region}a"
-    map_public_ip_on_launch = true
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-    tags {
-      managed = "terraform"
-      environment = "management"
-    }
-}
-resource "aws_subnet" "subnet-default-1b" {
-    vpc_id                  = "${aws_vpc.vpc-default.id}"
-    cidr_block              = "172.31.16.0/20"
-    availability_zone       = "${var.region}b"
-    map_public_ip_on_launch = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-    tags {
-      managed = "terraform"
-      environment = "management"
-    }
-}
-resource "aws_subnet" "subnet-default-1c" {
-    vpc_id                  = "${aws_vpc.vpc-default.id}"
-    cidr_block              = "172.31.32.0/20"
-    availability_zone       = "${var.region}c"
-    map_public_ip_on_launch = true
-
-    tags {
-      managed = "terraform"
-      environment = "management"
-    }
-}
-resource "aws_subnet" "subnet-default-1d" {
-    vpc_id                  = "${aws_vpc.vpc-default.id}"
-    cidr_block              = "172.31.0.0/20"
-    availability_zone       = "${var.region}d"
-    map_public_ip_on_launch = true
-
-    tags {
-      managed = "terraform"
-      environment = "management"
-    }
-}
-resource "aws_subnet" "subnet-default-1e" {
-    vpc_id                  = "${aws_vpc.vpc-default.id}"
-    cidr_block              = "172.31.48.0/20"
-    availability_zone       = "${var.region}e"
-    map_public_ip_on_launch = true
-
-    tags {
-      managed = "terraform"
-      environment = "management"
-    }
-}
-resource "aws_subnet" "subnet-default-1f" {
-    vpc_id                  = "${aws_vpc.vpc-default.id}"
-    cidr_block              = "172.31.64.0/20"
-    availability_zone       = "${var.region}f"
-    map_public_ip_on_launch = true
-
-    tags {
-      managed = "terraform"
-      environment = "management"
-    }
+  tags = {
+    Terraform = "true"
+    Environment = "management"
+  }
 }
