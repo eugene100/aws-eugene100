6eb3653c08ee013a7a65d56708ddf4703ca14250
Yevhen Stolietnii <eugene100@users.noreply.github.com>
2022-08-05 18:05:14 +0300

    Fix azs names (#3)

--- terraform-management/s3.tf
+++ terraform-management/s3.tf
@@ -2,14 +2,14 @@ data "aws_s3_bucket" "eugene100-terraform-tfstate" {
   bucket = "eugene100-terraform-tfstate"
 }
 
-# module "s3_bucket" {
-#   source = "terraform-aws-modules/s3-bucket/aws"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-#   bucket = "eugene100-k8s-state-store"
-#   acl    = "private"
+  bucket = "eugene100-k8s-state-store"
+  acl    = "private"
 
-#   versioning = {
-#     enabled = true
-#   }
+  versioning = {
+    enabled = true
+  }
 
-# }
+}
